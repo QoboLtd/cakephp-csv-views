0675671aebd6c796a114dcf980b886843788523a
George Constantinou <g.constantinou@qobo.biz>
2016-03-02 17:50:05 +0200

    implement inteface methods (task #1376)

--- src/FieldHandlers/BaseFieldHandler.php
+++ src/FieldHandlers/BaseFieldHandler.php
@@ -5,4 +5,18 @@ use CsvViews\FieldHandlers\FieldHandlerInterface;
 
 class BaseFieldHandler implements FieldHandlerInterface
 {
+    /**
+     * Method that renders default type field's value.
+     * @param  mixed  $table   name or instance of the Table
+     * @param  string $field   field name
+     * @param  string $data    field data
+     * @param  array  $options field options
+     * @return string
+     */
+    public function renderValue($table, $field, $data, array $options)
+    {
+        $result = '';
+
+        return $result;
+    }
 }
