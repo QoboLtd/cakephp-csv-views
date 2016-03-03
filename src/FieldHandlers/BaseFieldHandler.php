f3549f28454d628f6b6fe4802ff532997912f902
George Constantinou <g.constantinou@qobo.biz>
2016-03-03 14:58:54 +0200

    pass field type on Form input rendering (task #1376)

--- src/FieldHandlers/BaseFieldHandler.php
+++ src/FieldHandlers/BaseFieldHandler.php
@@ -7,6 +7,18 @@ use CsvViews\FieldHandlers\FieldHandlerInterface;
 class BaseFieldHandler implements FieldHandlerInterface
 {
     /**
+     * Csv field types respective input field types
+     * @var array
+     */
+    protected $_fieldTypes = [
+        'boolean' => 'checkbox',
+        'text' => 'textarea',
+        'string' => 'text',
+        'uuid' => 'text',
+        'integer' => 'number'
+    ];
+
+    /**
      * Method responsible for rendering field's input.
      * @param  mixed  $table   name or instance of the Table
      * @param  string $field   field name
@@ -15,11 +27,18 @@ class BaseFieldHandler implements FieldHandlerInterface
      */
     public function renderInput($table, $field, array $options = [])
     {
-        // load html helper
+        // load AppView
         $cakeView = new AppView();
-        $cakeView->loadHelper('Html');
 
-        return $cakeView->Form->input($field);
+        $fieldType = $options['fieldDefinitions']['type'];
+
+        if (in_array($fieldType, array_keys($this->_fieldTypes))) {
+            $fieldType = $this->_fieldTypes[$fieldType];
+        }
+
+        return $cakeView->Form->input($field, [
+            'type' => $fieldType
+        ]);
     }
 
     /**
