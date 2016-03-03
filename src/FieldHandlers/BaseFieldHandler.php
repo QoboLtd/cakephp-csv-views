8670b628de43d073f74b43cc3cb04e8013d030f3
George Constantinou <g.constantinou@qobo.biz>
2016-03-03 12:06:20 +0200

    implement renderInput method basic functionality (task #1376)

--- src/FieldHandlers/BaseFieldHandler.php
+++ src/FieldHandlers/BaseFieldHandler.php
@@ -1,10 +1,27 @@
 <?php
 namespace CsvViews\FieldHandlers;
 
+use App\View\AppView;
 use CsvViews\FieldHandlers\FieldHandlerInterface;
 
 class BaseFieldHandler implements FieldHandlerInterface
 {
+    /**
+     * Method responsible for rendering field's input.
+     * @param  mixed  $table   name or instance of the Table
+     * @param  string $field   field name
+     * @param  array  $options field options
+     * @return string          field input
+     */
+    public function renderInput($table, $field, array $options = [])
+    {
+        // load html helper
+        $cakeView = new AppView();
+        $cakeView->loadHelper('Html');
+
+        return $cakeView->Form->input($field);
+    }
+
     /**
      * Method that renders default type field's value.
      * @param  mixed  $table   name or instance of the Table
